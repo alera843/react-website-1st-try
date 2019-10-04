e702250082d5e5782bb631d56c37e517444ffff1
Neli Temelkova <neli.temelkova@notezapp.com>
2019-10-04 18:09:43 +0300

    Team section and contact form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 //Pages
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
+import Contact from './components/Pages/Contact';
 
 class App extends Component {
 render() {
@@ -16,12 +17,16 @@ render() {
 					path="/"
 					component= {Home}
 				/>
-				
+
 				<Route
 					path= "/about"
 					component= {About}
 				/>
 
+				<Route
+					path= "/contact"
+					component= {Contact}
+				/>
 		</PageWrapper>
 	</Router>
 	  );
